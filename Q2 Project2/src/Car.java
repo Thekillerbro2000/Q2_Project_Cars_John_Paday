ae33f9ff365b98469c1da71027950476f245c99f
1909676 <1909676@01-19765.pusd.dom>
2021-11-03 15:10:28 -0700

    Initial commit?

---
+++ Q2 Project2/src/Car.java
@@ -0,0 +1,72 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Car{
+	
+	//image related variables
+	public int x, y;
+	private Image img; 	
+	private AffineTransform tx;
+	private int vx = 0;
+	public Car(int x, int y) {
+		img = getImage("/imgs/bg.png"); //load the image for Tree
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+
+	
+	/* update variables here */
+	private void update() {
+
+		
+		
+	}
+	public void left() {
+		vx = -1;
+	}
+	public void right() {
+		vx = 1;
+	}
+	/* Drawing commands */
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+		//call update to update the actualy picture location
+		update();
+		
+		
+		
+		
+		g2.drawImage(img, tx, null);
+		
+		
+
+	}
+
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1.0, 1.0);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Car.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+}
